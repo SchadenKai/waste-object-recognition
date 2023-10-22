2d7a8329f053e1dba2446b3bf5351ba0aaa2705b
SchadenKai <kairustecsonpersonal@gmail.com>
2023-10-22 18:00:56 +0800

    tutorial shits

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from ultralytics import YOLO
 import cv2
 
-model =YOLO('yolov8n.py')
+model =YOLO('yolov8n.pt')
 
 video_path = './video/video.mp4'
-cv2.VideoCapture(video_path)
+cap = cv2.VideoCapture(video_path)
 
+ret = True
+
+while ret:
+    ret, frame = cap.read()
+    
+    results = model.track(frame, persist=True)
+    frame_ = results[0].plot()
+    cv2.imshow('frame', frame_)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
