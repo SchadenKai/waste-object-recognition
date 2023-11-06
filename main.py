8a9256fdfbe8151af8f03bdec86b3665fcc25ad8
SchadenKai <kairustecsonpersonal@gmail.com>
2023-11-06 22:10:03 +0800

    yeah big

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from ultralytics import YOLO
 import cv2
 
-model =YOLO('yolov8n.pt')
+model = YOLO('yolov8n.yaml')
+results = model.train(data="model_config.yaml", epochs=3)
 
-video_path = './video/video.mp4'
-cap = cv2.VideoCapture(video_path)
+# video_path = './video/video.mp4'
+# cap = cv2.VideoCapture(video_path)
 
-ret = True
+# ret = True
 
-while ret:
-    ret, frame = cap.read()
+# while ret:
+#     ret, frame = cap.read()
     
-    results = model.track(frame, persist=True)
-    frame_ = results[0].plot()
-    cv2.imshow('frame', frame_)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+#     results = model.track(frame, persist=True)
+#     frame_ = results[0].plot()
+#     cv2.imshow('frame', frame_)
+#     if cv2.waitKey(25) & 0xFF == ord('q'):
+#         break
+    
